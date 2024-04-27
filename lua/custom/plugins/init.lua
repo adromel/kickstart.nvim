46acda693f29b86cf0c115a6c11c040b1fd12e50
Lex Dromel <alexandre.dromel@gmail.com>
2024-04-27 04:35:19 +0200

    Added undotree

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,6 +3,14 @@
 --
 -- See the kickstart.nvim README for more information
 return {
+  {
+    'jiaoshijie/undotree',
+    dependencies = 'nvim-lua/plenary.nvim',
+    config = true,
+    keys = { -- load the plugin only when using it's keybinding:
+      { '<leader>u', "<cmd>lua require('undotree').toggle()<cr>" },
+    },
+  },
   vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv"),
   vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv"),
 }
