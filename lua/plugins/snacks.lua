e3e12918117b1b6ccf20e0ee00fc414ee6c75970
Alex Dromel <alexandre.dromel@gmail.com>
2025-10-22 22:55:09 +0200

    new startup

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,30 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    picker = {},
+  },
+  keys = {
+    -- Top Pickers & Explorer
+    {
+      "<leader><space>",
+      function()
+        Snacks.picker.recent({ focus = "list" })
+      end,
+      desc = "Recent",
+    },
+    {
+      "<leader>U",
+      function()
+        Snacks.picker.undo({ focus = "list" })
+      end,
+      desc = "Undo History",
+    },
+    {
+      "<leader>s.",
+      function()
+        Snacks.picker.files({ focus = "list" })
+      end,
+      desc = "Find Files",
+    },
+  },
+}
